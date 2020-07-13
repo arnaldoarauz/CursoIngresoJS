da9d04d961021a8787db5f65b29d24a43a9bec94
arnaldoarauz <arnaldoarauz1991@gmail.com>
2020-07-13 12:34:50 -0300

    clase 1 / 13/07

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,8 +1,8 @@
-/*
+﻿/*
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("hola mundo");
+	alert("Esto funciona de maravilla");
 }
 
