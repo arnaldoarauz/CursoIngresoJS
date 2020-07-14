2994386414ff42f30fa5edd286c9863a4597e575
arnaldoarauz <arnaldoarauz1991@gmail.com>
2020-07-14 12:04:19 -0300

    clase 2 - 14/07

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,6 +4,15 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
+	let numUno;
+	let numDos;
+	let resultado;
+
+	numUno = parseInt(document.getElementById("txtIdNumeroUno").value);
+	numDos = parseInt(document.getElementById("txtIdNumeroDos").value);
+
+	resultado = numUno + numDos;
+
+	alert("La suma es: " + resultado);
 }
 
--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -5,6 +5,13 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	let sueldo;
+	let nuevoSueldo;
 
-	alert("ok");
+	sueldo = parseFloat(document.getElementById("txtIdSueldo").value);
+
+	nuevoSueldo = sueldo + (sueldo * .1);
+
+	document.getElementById("txtIdResultado").value = nuevoSueldo;
+
 }
