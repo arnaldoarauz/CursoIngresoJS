faf8ef6fce13525fd0c052ca8a77af435133d303
arnaldoarauz <arnaldoarauz1991@gmail.com>
2020-07-20 12:21:13 -0300

    clase 4 - 20/07/2020

--- 2-InstruccionIf/jsInstruccionIF-06.js
+++ 2-InstruccionIf/jsInstruccionIF-06.js
@@ -1,7 +1,16 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edad;
+
+	edad = parseInt(document.getElementById("txtIdEdad").value);
+
+	if(edad >= 18){
+		alert("Es adulto");
+	}else if(edad >= 13){
+		alert("Es adolescente");
+	}else{
+		alert("Es niño");
+	}
 
 
 
